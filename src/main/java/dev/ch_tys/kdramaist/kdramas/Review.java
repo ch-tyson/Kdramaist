d591c3b14859a7f5ef0723c630b0aafa92d6f90b
ch-tyson <113953467+ch-tyson@users.noreply.github.com>
2023-02-16 02:21:39 +0000

    Review endpoints set

--- src/main/java/dev/ch_tys/kdramaist/kdramas/Review.java
+++ src/main/java/dev/ch_tys/kdramaist/kdramas/Review.java
@@ -1,5 +1,22 @@
 package dev.ch_tys.kdramaist.kdramas;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor @NoArgsConstructor
+
 public class Review {
-    
+    private ObjectId id;
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+        
+    }
 }
---
+++ src/main/java/dev/ch_tys/kdramaist/kdramas/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.ch_tys.kdramaist.kdramas;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
+
+}
