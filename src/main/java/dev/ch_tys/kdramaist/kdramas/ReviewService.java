beeca960a7faa328d16f95f1f4d02311b40cc34a
ch-tyson <cheungtyson@gmail.com>
2023-02-23 22:20:31 -0500

    Client reset

--- src/main/java/dev/ch_tys/kdramaist/kdramas/ReviewService.java
+++ src/main/java/dev/ch_tys/kdramaist/kdramas/ReviewService.java
@@ -1,5 +1,7 @@
 package dev.ch_tys.kdramaist.kdramas;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -8,21 +10,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
-
     @Autowired
     private ReviewRepository reviewRepository;
-
     @Autowired
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId) {
-        Review review = reviewRepository.insert(new Review(reviewBody));
+        Review review = reviewRepository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
 
         mongoTemplate.update(Kdrama.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
                 .first();
 
-                return review; 
+        return review;
     }
 }
